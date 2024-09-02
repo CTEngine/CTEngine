c828dc24153c12fa3215ba925aeeb57a393c8ec3
CTEngine <armond.domina@gmail.com>
2024-09-02 01:30:03 -0400

    CTEngine

--- projects/C++Projects/HandmadeHero/HandmadeHero.cpp
+++ projects/C++Projects/HandmadeHero/HandmadeHero.cpp
@@ -73,43 +73,49 @@ typedef struct tagWNDCLASSA
 
 LRESULT CALLBACK
 MainWindowCallback(HWND Window,
-                   UINT Message,
-                   WPARAM WParam,
-                   LPARAM LParam)
+                  UINT Message,
+                  WPARAM WParam,
+                  LPARAM LParam)
 {
 
   LRESULT Result = 0;
 
   // SWITCH STATMENT:
-  switch (Message)
+  switch(Message)
   {
-  case WM_SIZE:
-  {
-    OutputDebugStringA("WM_SIZE \n");
-  }
-  break;
-  case WM_DESTROY:
-  {
-    OutputDebugStringA("WM_DESTROY \n");
-  }
-  break;
-  case WM_CLOSE:
-  {
-    OutputDebugStringA("WM_CLOSE \n");
-  }
-  break;
-  case WM_ACTIVATEAPP:
-  {
-    OutputDebugStringA("WM_ACTIVATEAPP \n");
-  }
-  break;
+      case WM_SIZE:
+      {
+          OutputDebugStringA("WM_SIZE \n");
+      } break;
+      case WM_DESTROY:
+      {
+          OutputDebugStringA("WM_DESTROY \n");
+      } break;
+      case WM_CLOSE:
+      {
+          OutputDebugStringA("WM_CLOSE \n");
+      } break;
+      case WM_ACTIVATEAPP:
+      {
+          OutputDebugStringA("WM_ACTIVATEAPP \n");
+      } break;
+      case WM_PAINT:
+      {
+          PAINTSTRUCT Paint;
+          HDC DeviceContext = BeginPaint(Window, &Paint);
+          int X = Paint.rcPaint.left;
+          int Y = Paint.rcPaint.top;
+          int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+          int Height = Paint.rcPaint.bottom = Paint.rcPaint.top;
+          PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+          EndPaint(Window, &Paint);
+      } break;
 
-  default:
-  {
-    // OutputDebugStringA("WM_DEFAULT \n");
-    Result = DefWindowProc(Window, Message, WParam, LParam);
-  }
-  break;
+      default:
+      {
+          // OutputDebugStringA("WM_DEFAULT \n");
+          Result = DefWindowProc(Window, Message, WParam, LParam);
+      } break;
   }
 
   return (Result);
